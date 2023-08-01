583a65d4c103ad1e729c7bd4bfd0bca709ff4184
Benson Kiai <benson.kiai86@gmail.com>
2023-08-02 00:55:27 +0300

    Fix Linters Errors

--- src/Modules/tvApi.js
+++ src/Modules/tvApi.js
@@ -1,11 +1,10 @@
-const myUrl = 'https://api.tvmaze.com/shows/'
+const myUrl = 'https://api.tvmaze.com/shows/';
 
 const container = document.getElementById('cardContainer');
 
-const display = (json) => {
-  
-  document.body.appendChild(myImg)
-}
+// const display = (json) => {
+//   document.body.appendChild(myImg)
+// }
 
 const createCard = (obj, json) => {
   // Create a div element with class card
@@ -18,8 +17,8 @@ const createCard = (obj, json) => {
 
   // Create a div element with class img-container
   const imgContainer = document.createElement('div');
-    imgContainer.className = 'img-container';
-    //Create an image
+  imgContainer.className = 'img-container';
+  //Create an image
   const myImg = document.createElement('img');
   myImg.src = json.image.original;
 
@@ -33,28 +32,24 @@ const createCard = (obj, json) => {
   commentBtn.textContent = 'Comment';
 
   // Append the title and the imgContainer to the card
-    imgContainer.appendChild(myImg)
-    card.appendChild(title); ;
+  imgContainer.appendChild(myImg);
+  card.appendChild(title);
   card.appendChild(imgContainer);
   card.appendChild(likeBtn);
   card.appendChild(commentBtn);
   container.appendChild(card);
 
-  
-    
-
   // Return the card element
-//   return card;
+  //   return card;
 };
 
 export const fetchTvApi = async (obj) => {
-  const response = await fetch(myUrl+obj.id);
+  const response = await fetch(myUrl + obj.id);
   const json = await response.json();
   if (json) {
     createCard(obj, json);
   }
 };
-
 
 // const myArr = [{
 //   "name": "Game of Thrones",
