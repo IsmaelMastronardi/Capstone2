27f8eff9b6024a24b86ad0f464a1c60a180979ba
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-02 13:26:28 -0300

    add popup menu

--- src/Modules/tvApi.js
+++ src/Modules/tvApi.js
@@ -34,14 +34,67 @@ const createCard = (json) => {
   card.appendChild(commentBtn);
   container.appendChild(card);
 };
-
+let arr = [];
 const fetchTvApi = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const json = await response.json();
   if (json) {
     const slicedJson = json.slice(0, 10);
+    arr = slicedJson;
     slicedJson.forEach((elem) => { createCard(elem); });
   }
 };
+
+const removeMenu = (menu) => {
+  document.body.removeChild(menu);
+  document.body.style.overflow = 'scroll';
+};
+
+const displayMenu = (btnid) => {
+  console.log(arr);
+  const id = btnid.replace(/\D+/, '') - 1;
+  const popupMenu = document.createElement('section');
+  popupMenu.classList.add('popupMenuBackground');
+  popupMenu.innerHTML = `
+  <div class="popupMenu">
+  <button class="exitBtn" id="popupExitBtn">X</button>
+  <div>
+    <img src="${arr[id].image.medium}" class="popupMenuImg">
+  </div>
+  <div class="movie">
+    <h3 class="movieTitle">${arr[id].name}</h3>
+    <div class="infoDif">
+      <p class="movieInfo">genres : ${arr[id].genres}</p>
+      <p class="movieInfo">status : ${arr[id].status}</p>
+    </div>
+    <div class="infoDif">
+    <p class="movieInfo">network : ${arr[id].network.name}</p>
+    <p class="movieInfo">raiting : ${arr[id].raiting}</p>
+  </div>
+  </div>
+  <div class="comments">
+    <title>Comments : 1</title>
+    <p>first comment</p>
+    <p>second commnet</p>
+  </div>
+  <form>
+    <title>Add A Comment</title>
+    <input placeholder="Your name">
+    <input placeholder="Your insigths">
+    <button>Comment</button>
+  </form>
+</div>
+  `;
+  document.body.appendChild(popupMenu);
+  document.body.style.overflow = 'hidden';
+  const popupExitBtn = document.querySelector('#popupExitBtn');
+  popupExitBtn.addEventListener('click', () => { removeMenu(popupMenu); });
+};
+
+container.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Comment') {
+    displayMenu(e.target.id);
+  }
+});
 
 export default fetchTvApi;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import './css/popup.css';
 import fetchTvApi from './Modules/tvApi.js';
 
 window.onload = fetchTvApi();
