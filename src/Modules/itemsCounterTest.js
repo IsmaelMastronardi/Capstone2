1f948e52b661346197180ef15859a7b5ebd09d8c
Benson Kiai <benson.kiai86@gmail.com>
2023-08-04 19:42:12 +0300

    Fix ESlinter errors

--- src/Modules/itemsCounterTest.js
+++ src/Modules/itemsCounterTest.js
@@ -4,15 +4,15 @@ import countItems from './itemsCounter.js';
 
 // Mock the DOM environment using JSDOM
 const dom = new JSDOM(
-  '<html>' +
-    '<body>' +
-    '<div class="card"></div>' +
-    '<div class="card"></div>' +
-    '<div class="card"></div>' +
-    '<div class="card"></div>' +
-    '<div class="items-counter"></div>' +
-    '</body>' +
-    '</html>'
+  '<html>'
+    + '<body>'
+    + '<div class="card"></div>'
+    + '<div class="card"></div>'
+    + '<div class="card"></div>'
+    + '<div class="card"></div>'
+    + '<div class="items-counter"></div>'
+    + '</body>'
+    + '</html>',
 );
 global.document = dom.window.document;
 
