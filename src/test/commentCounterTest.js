251e8cc631b0c54da05a295a74c3d36db4a2b46e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-04 21:22:35 -0300

    add babel and change tests

--- src/test/commentCounterTest.js
+++
@@ -1,5 +0,0 @@
-const commentCounter = (commentsDiv, commentsTitle) => {
-  const num = commentsDiv.childElementCount;
-  commentsTitle.textContent = `Comments : ${num}`;
-};
-module.exports = commentCounter;
